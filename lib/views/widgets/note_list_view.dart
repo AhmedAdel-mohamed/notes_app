b38453d1635561b7bd4bbe0c547a8bd18ae5170b
Ahmed Adel <ahmedadel@gmail.com>
2026-02-10 00:29:06 +0200

    create notes list view

---
+++ lib/views/widgets/note_list_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_note_item.dart';
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context,index){
+        return Padding(
+          padding: const EdgeInsets.symmetric( vertical: 8),
+          child: const NoteItem(),
+        );
+      });
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_note_item.dart';
 import 'package:notes_app/views/widgets/cutom_app_bar.dart';
+import 'package:notes_app/views/widgets/note_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -10,9 +11,10 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children: const [SizedBox(height: 50), CustomAppBar(), NoteItem()],
+        children: const [SizedBox(height: 50), CustomAppBar(), Expanded(child: NoteListView())],
       ),
     );
   }
 }
+
 
